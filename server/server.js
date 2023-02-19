1d8ff2d63490c9a74e066a94e3cfaea47e08202c
zedlikae <zedlikakevinly@outlook.com>
2023-02-18 23:37:15 -0600

    update the server url

--- server/server.js
+++ server/server.js
@@ -28,11 +28,14 @@ app.post('/', async (req, res) => {
         const prompt = req.body.prompt;
 
         const response = await openai.createCompletion({
-            model: 'text-davinci-003',
+            model: "text-davinci-003",
             prompt: `${prompt}`,
             max_tokens: 3000,
             n: 1,
-        });
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
+          });
 
         res.status(200).send({
             bot: response.data.choices[0].text,
